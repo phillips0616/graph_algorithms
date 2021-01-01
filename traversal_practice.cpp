f0c70008da4920589d0fb23d354e7266620a4ef4
Phillip Smith <psmith0616@gmail.com>
2021-01-01 12:42:25 -0500

    added function to count graph connections

--- traversal_practice.cpp
+++ traversal_practice.cpp
@@ -53,13 +53,49 @@ vector<int> shortestPathBFS(int start, int target, vector<vector<int>> graph) {
   return sp;
 }
 
+vector<int> connections(vector<vector<int>> graph) {
+  vector<bool> visited(graph.size(), false);
+  vector<int> connections(graph.size(), -1);
+  queue<int> q;
+  int connectValue = 1;
+  int parent;
+  int curVertex;
+  cout << "starting..." << endl;
+  for(int i = 0; i < visited.size(); i++) {
+    if(!visited[i]) {
+      parent = i;
+      q.push(parent);
+      visited[parent] = true;
+      connections[parent] = connectValue;
+      cout << "visiting " << parent << endl;
+      while(!q.empty()) {
+        parent = q.front();
+        cout << "parent is " << parent << endl;
+        q.pop();
+        for(int j = 0; j < graph[parent].size(); j++) {
+          curVertex = graph[parent][j];
+          if(!visited[curVertex]) {
+            cout << "visiting neighbor " << parent << endl;
+            visited[curVertex] = true;
+            connections[curVertex] = connectValue;
+            q.push(curVertex);
+          }
+        }
+      }
+      connectValue++; 
+    }
+  }
+  return connections;
+}
+
 int main() {
   GraphBuilder gb = GraphBuilder();
   gb.buidGraphFromFile("graph.txt");
 
-  vector<int> shortPath = shortestPathBFS(0,3,gb.getGraph());
+  //vector<int> shortPath = shortestPathBFS(0,3,gb.getGraph());
+  vector<int> connectionsVector = connections(gb.getGraph());
 
-  for(int x : shortPath) {
+  for(int x : connectionsVector) {
     cout << x << endl;
   }
 
